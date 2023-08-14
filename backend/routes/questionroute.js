579358c3949bd5665a3fb990d7e3c939004d50f8
Kosuri Ravikanth <kosuriravikanth@gmail.com>
2023-08-14 13:38:30 +0530

    Update questionroute.js

--- backend/routes/questionroute.js
+++ backend/routes/questionroute.js
@@ -2,12 +2,11 @@ const express = require('express');
 const quesRouter = express.Router();
 const quesModel = require('../model/questionmodel');
 
+// Create a new question
 quesRouter.post('/ques', async (req, res) => {
   try {
     const { email, surveyId, questionText, option } = req.body;
-
-    console.log(req.body);
-
+    
     const newQues = new quesModel({
       email,
       surveyId,
@@ -17,7 +16,6 @@ quesRouter.post('/ques', async (req, res) => {
 
     const result = await newQues.save();
 
-    console.log(result);
     res.status(200).json({
       status: 'Success',
       message: 'MCQ question created successfully',
@@ -30,31 +28,33 @@ quesRouter.post('/ques', async (req, res) => {
     });
   }
 });
+
+// Get all questions
 quesRouter.get('/ques', async (req, res) => {
   try {
-    let allTheme = await quesModel.find()
+    const allQuestions = await quesModel.find();
     res.status(200).json({
-      status:"Success",
-      result : allTheme
-  })
+      status: 'Success',
+      result: allQuestions,
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
+// Get questions by surveyId
 quesRouter.get('/ques/:surveyId', async (req, res) => {
   const surveyId = req.params.surveyId;
   try {
-    const ques = await quesModel.find({ surveyId: surveyId });
-console.log(ques);
+    const questions = await quesModel.find({ surveyId: surveyId });
     res.status(200).json({
-      status: "Success",
-      result: ques
+      status: 'Success',
+      result: questions,
     });
   } catch (err) {
     res.status(400).json({
-      status: "Failed",
-      message: err.message
+      status: 'Failed',
+      message: err.message,
     });
   }
 });
