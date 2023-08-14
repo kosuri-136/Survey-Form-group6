90af93b76bdb186076a34c709a2ebf322d1f4fd6
G DEVI SURYA PRAKASH <116092584+Devisurya58@users.noreply.github.com>
2023-08-14 15:47:29 +0530

    Update SideBar.js

--- frontend/src/Components/SideBar/SideBar.js
+++ frontend/src/Components/SideBar/SideBar.js
@@ -1,42 +1,52 @@
-import React from 'react'
+
+
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Sidebar.css'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faUsers,
-    faTableList,
-    faHouse,
-} from "@fortawesome/free-solid-svg-icons";
+import './Sidebar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUsers, faTableList, faHouse } from '@fortawesome/free-solid-svg-icons';
 import Navcommon from '../Navbar/Navbar';
 import Surveylist from '../Surveylist/Surveylist';
 
 function Sidebar() {
-    const navigate = useNavigate();
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    navigate('/')
-  }
-    return (
+  const navigate = useNavigate();
+  const [activeIcon, setActiveIcon] = useState(null); // State to track active icon
 
-        <>
-        <div className="sidebar">
-            <ul>
-                <li className='home' onClick={handleSubmit}>
-                    <FontAwesomeIcon icon={faHouse} />
-                </li>
-                <li >
-                <FontAwesomeIcon icon={faUsers} />
-                    
-                </li>
-                <li>
-                    <FontAwesomeIcon icon={faTableList} />
-                </li>
-            </ul>
-        </div>
-   
+  const handleIconClick = (iconName) => {
+    setActiveIcon(iconName); // Set active icon state
+
+    if (iconName === 'home') {
+      navigate('/');
+    } else if (iconName === 'preview') {
+      navigate('/preview');
+    } else if (iconName === 'list') {
+      navigate('/surveylist');
+    }
+  };
+
+  return (
+    <>
+      <div className="sidebar">
+        <ul>
+          <li className={activeIcon === 'home' ? 'active' : ''} onClick={() => handleIconClick('home')}>
+            <div>
+              <FontAwesomeIcon icon={faHouse} />
+            </div>
+          </li>
+          <li className={activeIcon === 'preview' ? 'active' : ''} onClick={() => handleIconClick('preview')}>
+            <div>
+              <FontAwesomeIcon icon={faUsers} />
+            </div>
+          </li>
+          <li className={activeIcon === 'list' ? 'active' : ''} onClick={() => handleIconClick('list')}>
+            <div>
+              <FontAwesomeIcon icon={faTableList} />
+            </div>
+          </li>
+        </ul>
+      </div>
     </>
-
-    )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
