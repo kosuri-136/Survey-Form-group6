2a9099cbd9caaa334370733a07eedcd8356ff044
rakesh <126374141+rakeshjson@users.noreply.github.com>
2023-08-17 10:39:07 +0530

    fixed Logout error after Logout.js
    when an user Logout and press the back button itshows the previous logged in page; with this code this error is fixed

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -2,18 +2,22 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css"; // You can create a CSS file for styling
-import Image from "../profile-pic.jpg"; // 
+import Image from "../profile-pic.jpg"; 
 import { useNavigate } from 'react-router-dom';
 const Navcommon = () => {
   const [showLogoutMenu, setShowLogoutMenu] = useState(false);
   const navigate = useNavigate();
   const handleLogout = () => {
    
-    
         localStorage.clear()
         navigate("/");
-    
   };
+
+    // function to prevent acess to loginPages after LOGOUT
+    window.addEventListener('popstate', function(event) { 
+    navigate("/");
+  });
+
 
   return (
     <nav className="navbar">
